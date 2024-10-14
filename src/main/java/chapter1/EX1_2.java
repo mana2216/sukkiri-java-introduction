760514d4d3a5b8f97802352da725ed7dde91b120
nagasawa <ms00lx.221b@gmail.com>
2024-10-14 12:01:42 +0900

    スッキリわかるJava入門第4版の第1章まで終了

---
+++ src/main/java/chapter1/EX1_2.java
@@ -0,0 +1,8 @@
+package chapter1;
+
+public class EX1_2 {
+    public static void main(String[] args) {
+        int area = 3 * 5;
+        System.out.println("縦幅3横幅5の長方形の面積は、" + area);
+    }
+}
---
+++ src/main/java/chapter1/EX1_3.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class EX1_3 {
+    public static void main(String[] args) {
+        boolean flag = true;
+        char ch = '駆';
+        double val1 = 3.14;
+        long val2 = 314159265853979L;
+        String strs = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
